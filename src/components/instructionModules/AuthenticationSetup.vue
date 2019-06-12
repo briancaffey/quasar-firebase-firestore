9f6ccf15f9bc4c1dd79182972c65a3e5a4e66beb
birchb <birchb@users.noreply.github.com>
2019-06-11 23:36:57 -0400

    Update AuthenticationSetup.vue
    update command to create new boot extension.
    Fix several typos.

--- src/components/instructionModules/AuthenticationSetup.vue
+++ src/components/instructionModules/AuthenticationSetup.vue
@@ -9,7 +9,7 @@
       <q-card-section>
         <p class="text-body1">
           Ok, so we have firebase setup in the console, and our api keys sitting in our .env.dev file, but now we need to make two methods that will allow us to create new users and log them in as well as setup our configuration to initialize the firebase app. First lets create a new boot file called "firebase".
-          <pre class="bash">$ quasar create boot firebase</pre>
+          <pre class="bash">$ quasar new boot firebase</pre>
         </p>
 
         <p class="text-body1">In our firebase boot file we're going to import firebase from the firebase/app folder in the firebase package, and in order for authentication to work we'll need to also require firebase/auth.
@@ -32,7 +32,7 @@
 
         <p class="text-body1">Now that the config is set up lets look at the two methods we need to get the users created and logged in.(boot/)</p>
 
-        <p class="text-body1">Both of these methods use the Firebase API methods: <em>signInWithEmailAndPassword</em> & <em>createUserWithEmailAndPassword</em>. These methods are asynchronus in nature, and there are wrapped in promises to handle the respective resolve and reject outcomes.
+        <p class="text-body1">Both of these methods use the Firebase API methods: <em>signInWithEmailAndPassword</em> & <em>createUserWithEmailAndPassword</em>. These methods are asynchronous in nature, and they are wrapped in promises to handle the respective resolve and reject outcomes.
           <pre class="bash--block q-pa-sm"><strong>/src/boot/firebase.js:15-37</strong>
   Vue.prototype.$login = (email, password) => {
     return new Promise((resolve, reject) => {
@@ -59,7 +59,7 @@
   }</pre>
         </p>
 
-        <p class="text-body1">By assigning thes method signatures to the Vue instance via the prototype property we can now use these any where in our app and pass up the outcomes of these methods to whatever components you may choose to use them in.</p>
+        <p class="text-body1">By assigning these method signatures to the Vue instance via the prototype property we can now use these anywhere in our app and pass up the outcomes of these methods to whatever components you may choose to use them in.</p>
 
         <p class="text-body1">Finally assigning the firebase instance to the Vue instance again for convienence to access the Firebase API elsewhere in our app by simply accessing it like this: <em>this.$fb</em>
           <br><strong>/src/boot/firebase.js:44</strong>
